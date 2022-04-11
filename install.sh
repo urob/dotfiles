ff96e6003676a7fa7002b75c7df23bd861d16dfd
urob <private>
2022-04-11 11:59:06 -0400

    Check for required programs

--- install.sh
+++ install.sh
@@ -22,14 +22,23 @@ mkdir -p $XDG_STATE_HOME/zsh
 mkdir -p $XDG_CACHE_HOME/zsh
 mkdir -p $VIMCONFIG/sessions
 
+# check whether command exists
+function command_exists () {
+    if ! command -v $1 &> /dev/null; then
+        echo "$(tput setaf 1)Error: $1 is not installed.$(tput sgr0)" >&2
+        exit 1
+    fi
+}
+
 # install 3rd party tools, usage: get_git user project install_dir
 function get_git() {
     rm -rf ${3}
     mkdir -p ${3}
-    git clone --depth=1 https://github.com/${1}/${2} ${3}
+    gituu clone --depth=1 https://github.com/${1}/${2} ${3}
 }
 
 # diff-so-fancy
+$(command_exists git) || exit 1
 get_git so-fancy diff-so-fancy $XDG_LIB_HOME/diffsofancy
 chmod +x $XDG_LIB_HOME/diffsofancy/diff-so-fancy
 ln -sf $XDG_LIB_HOME/diffsofancy/diff-so-fancy $XDG_BIN_HOME/diff-so-fancy
@@ -38,7 +47,11 @@ ln -sf $XDG_LIB_HOME/diffsofancy/diff-so-fancy $XDG_BIN_HOME/diff-so-fancy
 get_git romkatv gitstatus $GITSTATUSDIR
 
 # plug-vim
+$(command_exists curl) || exit 1
 curl -fLo "${VIMCONFIG}"/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 nvim --headless +PlugInstall +qall
+
+# reminder to reload tmux and zsh for changes to take effect
+echo "$(tput setaf 2)All done. Reload tmux and zsh config for settings to take effect.$(tput sgr0)"
 
