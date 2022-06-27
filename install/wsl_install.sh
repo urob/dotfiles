9165e0fd87da392bce54d9b1fe2d8bde9a95074d
urob <978080+urob@users.noreply.github.com>
2022-06-27 19:03:05 -0400

    Clean up home directory before install

--- install/wsl_install.sh
+++ install/wsl_install.sh
@@ -9,6 +9,8 @@ sudo apt-get update
 sudo apt-get install git
 
 # clone remote dotfiles dir
+rm -rf ${DOTFILES}
+mkdir -p ${DOTFILES}
 git clone ${DOTFILES_REMOTE} ${DOTFILES}
 
 # upgrade to bookworm
@@ -19,6 +21,13 @@ sudo apt autoremove
 
 # install packages from packages.lst (must have unix file endings)
 sudo apt-get install $(cat $DOTFILES/install/packages.lst)
+
+# clean up existing dotfiles
+files=$(find $HOME -maxdepth 1 -name ".*" -print | sort)$
+for f in $files; do
+    read -e -p "Delete ${f/$HOME/\~}? [y/N] " YN
+    [[ ${YN} =~ ^[Yy]$ ]] && rm -rf ${f}
+done
 
 # link dotfiles to home directory
 source $DOTFILES/install/install.sh
