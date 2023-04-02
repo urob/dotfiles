7d7923568d96f5193bbc83e8037f6f616855408f
urob <978080+urob@users.noreply.github.com>
2023-04-01 23:47:14 -0400

    Tweak wsl install

--- install/wsl_install.sh
+++ install/wsl_install.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
-## Run the following to bootstrap environment in a fresh Debian installation:
-# curl -fLo ~/wsl_install.sh https://raw.githubusercontent.com/urob/dotfiles/main/install/wsl_install.sh
-# chmod +x ~/wsl_install.sh && ~/wsl_install.sh
+# Run to bootstrap wsl config on a fresh Debian installation:
+#   curl https://raw.githubusercontent.com/urob/dotfiles/main/install/wsl_install.sh | bash
 
 # location of dotfiles
 DOTFILES=$HOME/dotfiles
@@ -11,7 +10,7 @@ DOTFILES_REMOTE=https://github.com/urob/dotfiles
 # make sure git is installed
 if ! command -v git &> /dev/null; then
     sudo apt-get update
-    sudo apt-get install git
+    sudo apt-get -y install git
 fi
 
 # clone remote dotfiles repository
@@ -19,14 +18,17 @@ rm -rf ${DOTFILES}
 mkdir -p ${DOTFILES}
 git clone ${DOTFILES_REMOTE} ${DOTFILES}
 
-# upgrade Debian to bookworm
-sudo cp ${DOTFILES}/install/sources.list /etc/apt/sources.list
+# upgrade Debian to sid
+sudo cp "$DOTFILES/install/sources.list" /etc/apt/sources.list
 sudo apt-get update
-sudo apt-get dist-upgrade
-sudo apt autoremove
+sudo apt-get -y install apt-listbugs
+sudo apt-get -y upgrade       # do minimal upgrade first
+sudo apt-get -y dist-upgrade  # then do full upgrade
+sudo apt-get -y autoremove
 
 # install Debian packages (packages.lst must have unix file endings)
-sudo apt-get install $(cat $DOTFILES/install/packages.lst)
+sudo apt-get -y install $(cat $DOTFILES/install/packages.lst)
+sudo apt-get clean
 
 # clean up existing dotfiles
 files=$(find $HOME -maxdepth 1 -name ".*" -print | sort)$
@@ -36,11 +38,12 @@ for f in $files; do
 done
 
 # install dotfiles
-source $DOTFILES/install/install.sh
+source $DOTFILES/install/dot_install.sh
 
 # make zsh the default shell
 chsh -s $(which zsh)
 
-# install miniconda
-# curl -fLo ~/conda_install.sh https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-# bash ~/conda_install.sh
+# Activate systemd and fix interop bug  https://github.com/microsoft/WSL/issues/8843
+sudo cp "$DOTFILES/install/wsl.conf" /etc/wsl.conf
+sudo sh -c 'echo :WSLInterop:M::MZ::/init:PF > /usr/lib/binfmt.d/WSLInterop.conf'
+
