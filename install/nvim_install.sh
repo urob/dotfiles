0b5bcab0b0e9fe299174aff5109e53682dc944a5
urob <978080+urob@users.noreply.github.com>
2023-08-13 09:55:32 -0400

    install(nvim): switch to appimage

--- install/nvim_install.sh
+++ install/nvim_install.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
 
 # get latest nightly build
-SRC=https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.deb
-curl -fLo /tmp/nvim.deb $SRC
-sudo apt install /tmp/nvim.deb
+# SRC=https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.deb
+# curl -fLo /tmp/nvim.deb $SRC
+# sudo apt install /tmp/nvim.deb
+SRC=https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+APP=$HOME/.local/appimage
+BIN=$HOME/.local/bin
+mkdir -p $APP
+mkdir -p $BIN
+curl -fLo $APP/nvim.appimage $SRC
+ln -sf $APP/nvim.appimage $HOME/.local/bin/nvim
 
 # install python support
-sudo apt-get update
-sudo apt-get install python3-neovim
+# sudo apt-get update
+# sudo apt-get install python3-neovim
 
