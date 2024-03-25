45ee9e7275c615028243c0e5eeca834b92eb6684
urob <978080+urob@users.noreply.github.com>
2024-03-25 15:17:30 -0400

    vim: manage plugins with nix

--- pkgs.nix
+++ pkgs.nix
@@ -16,7 +16,6 @@
     htop
     lf
     # man
-    neovim
     ripgrep
     sd
     tldr
@@ -51,13 +50,51 @@
     podman
     podman-compose
 
-  # Python
+    # Python
     micromamba
     # pipx
     python3
     ruff
 
-  # WSL
-  # xsel
+    # WSL
+    #(import ./win32yank.nix {inherit pkgs;})
   ];
+
+  programs.neovim = {
+    enable = true;
+    plugins = with pkgs.vimPlugins; [
+      # Colorscheme
+      onedark-nvim
+
+      # Core plugins
+      comment-nvim # or vim-commentary
+      fzf-vim
+      quickfix-reflector-vim
+      indent-blankline-nvim
+      vim-matchup
+      vim-repeat
+      vim-subversive
+      vim-surround
+      vim-tmux-navigator
+      # vim-zoom TODO: make flake
+      virt-column-nvim
+      which-key-nvim
+
+      # Git
+      conflict-marker-vim
+      vim-fugitive
+      gitsigns-nvim
+
+      # Treesitter
+      nvim-treesitter.withAllGrammars
+      # nvim-treesitter-refactor
+      nvim-treesitter-textobjects
+      nvim-ts-context-commentstring
+
+      # Lazy-loaded plugins
+      { plugin = markdown-preview-nvim; optional = true; }
+      { plugin = nerdtree; optional = true; }
+      { plugin = vim-mundo; optional = true; }
+    ];
+  };
 }
